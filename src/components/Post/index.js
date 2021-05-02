ee6f0d63bb5bac4f08858a322d4c4d54949e1b4d
frunox <jrcannon@msn.com>
2021-05-02 16:55:21 -0500

    center Post title

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -38,22 +38,26 @@ function Post(props) {
 
   return (
     <React.Fragment>
-      <HomeNav />
-      <div className="post-content">
-        <h1>{postArray[id].title}</h1>
-        {/* <h3>Author: {postArray[id].author}</h3> */}
-        <small>Published on {postArray[id].date}</small>
-        {/* <hr /> */}
-        <ReactMarkdown skipHtml={true} linkTarget={'_blank_'}>
-          {postArray[id].content}
-        </ReactMarkdown>
-        <p>
-          Thank you for reading my blog. Please send comments or suggestions for
-          future posts to <span> </span>
-          <a href="mailto:john@acodersquest.com">john@acodersquest.com.</a>
-        </p>
-      </div>
-      <Footer />
+      <section>
+        <HomeNav />
+        <main>
+          <div className="post-content">
+            <h1>{postArray[id].title}</h1>
+            {/* <h3>Author: {postArray[id].author}</h3> */}
+            <small>Published on {postArray[id].date}</small>
+            {/* <hr /> */}
+            <ReactMarkdown skipHtml={true} linkTarget={'_blank_'}>
+              {postArray[id].content}
+            </ReactMarkdown>
+            <p>
+              Thank you for reading my blog. Please send comments or suggestions
+              for future posts to <span> </span>
+              <a href="mailto:john@acodersquest.com">john@acodersquest.com.</a>
+            </p>
+          </div>
+        </main>
+        <Footer />
+      </section>
     </React.Fragment>
   );
 }
